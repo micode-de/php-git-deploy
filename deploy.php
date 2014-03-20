b1cb37cbf5aafeeff93137ff435d1cc98424efc9
Philipp Speck <philipp.speck@micode.de>
2014-03-20 15:04:28 +0100

    Added deploy.php

---
+++ deploy.php
@@ -0,0 +1,20 @@
+<?php
+
+//Definitions 
+//Change if required
+
+//Temporery Directory - Files will be cahched here
+define("TEMP_DIR", "C:/Users/speck_000/sandbox/git-deploy");
+
+//Repositorys - Could also be fetched from Database
+$repo = $_POST["repo"];
+$repos = array(
+			
+			0 => array( "repo" => "",
+						"" => ""
+				),
+			1 => array()
+		);
+
+
+?>
